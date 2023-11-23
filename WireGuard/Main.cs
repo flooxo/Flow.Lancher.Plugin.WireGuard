86d9a92f40db92747fda70a615946b2e6518d1b6
Florian Grabmeier <flo.grabmeier@gmail.com>
2023-11-23 19:41:19 +0100

    Basic logic  * show all possibles tunnels  * action to connect (ERROR)
    Signed-off-by: Florian Grabmeier <flo.grabmeier@gmail.com>

--- WireGuard/Main.cs
+++ WireGuard/Main.cs
@@ -1,7 +1,9 @@
 ﻿using System.Collections.Generic;
 using Flow.Launcher.Plugin;
+using System.IO;
+using System.Diagnostics;
 
-namespace HelloWorldCSharp
+namespace Flow.Launcher.Plugin.WireGuard
 {
     public class Main : IPlugin, IPluginI18n
     {
@@ -9,19 +11,56 @@ namespace HelloWorldCSharp
 
         public List<Result> Query(Query query)
         {
-            var result = new Result
+            //todo: get all wireguard connections
+            // TODO: verschieben nach init, variabel machen
+            // path can be .conf or .conf.dpapi
+            var tunnelPaths = @"C:\Program Files\WireGuard\Data\Configurations\";            
+            var filePaths = Directory.GetFiles(tunnelPaths);
+
+            var resultList = new List<Result>();
+            foreach (var filePath in filePaths)
             {
-                Title = "WIREGUARD VPN PLUGIN TEST",
-                SubTitle = $"Query: {query.Search}",
-                Action = c =>
+                resultList.Add(new Result
                 {
-                    Context.API.ShowMsg(Context.API.GetTranslation("plugin_helloworldcsharp_greet_title"),
-                                            Context.API.GetTranslation("plugin_helloworldcsharp_greet_subtitle"));
-                    return true;
-                },
-                IcoPath = "Images/wireguard.png"
-            };
-            return new List<Result> { result };
+                    Title = Path.GetFileNameWithoutExtension(filePath),
+                    SubTitle = filePath,
+                    IcoPath = "Images/wireguard.png",
+                    Action = e =>
+                    {
+                        //TODO: as action, connect/disconnect to wireguard connection
+                        var command = $"wireguard.exe /installtunnelservice \"{filePath}.dpapi\"";
+                        ProcessStartInfo info = new()
+                        {
+                            FileName = "cmd.exe",
+                            Verb = "runas",
+                            //Arguments = $"/c {command}",
+                            UseShellExecute = true,
+                            WindowStyle = ProcessWindowStyle.Hidden
+                        };
+
+                        Process.Start(info);
+
+                        return true;
+                    }
+                });
+            }
+
+            // var result = new Result
+            // {
+            //     Title = "WIREGUARD",
+            //     SubTitle = $"tunnelPaths: {tunnelPaths}",
+            //     IcoPath = "Images/wireguard.png"
+            //     Action = c =>
+            //     {
+            //         //todo: as action, connect/disconnect to wireguard connection
+            //         Context.API.ShowMsg(Context.API.GetTranslation("plugin_helloworldcsharp_greet_title"),
+            //                                 Context.API.GetTranslation("plugin_helloworldcsharp_greet_subtitle"));
+            //         return true;
+            //     },
+            // };
+            // return new List<Result> { result };
+
+            return resultList;
         }
 
         public void Init(PluginInitContext context)
