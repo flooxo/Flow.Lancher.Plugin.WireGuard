e10a1fcc875d75187c47c9d6b1dfd4efc775af0d
Florian Grabmeier <flo.grabmeier@gmail.com>
2023-11-30 11:04:28 +0100

    Refactor
    Signed-off-by: Florian Grabmeier <flo.grabmeier@gmail.com>

--- Flow.Launcher.Plugin.WireGuard/Main.cs
+++ Flow.Launcher.Plugin.WireGuard/Main.cs
@@ -1,7 +1,9 @@
-﻿using System.Collections.Generic;
-using Flow.Launcher.Plugin;
+﻿using Flow.Launcher.Plugin;
+using System.Collections.Generic;
 using System.IO;
 using System.Diagnostics;
+using System;
+using System.Collections;
 
 namespace Flow.Launcher.Plugin.WireGuard
 {
@@ -9,37 +11,26 @@ namespace Flow.Launcher.Plugin.WireGuard
     {
         internal PluginInitContext Context;
 
-        private const string Image = $"Images\wireguard.png";
+        private const string Image = @"Images\wireguard.png";
 
-        private string tunnelPaths = @"C:\Program Files\WireGuard\Data\Configurations\";
+        private string configPath = @"C:\Program Files\WireGuard\Data\Configurations\";
+        private string[] tunnelPaths = Array.Empty<string>();
 
         public List<Result> Query(Query query)
         {
-            var filePaths = Directory.GetFiles(tunnelPaths);
+            var resultList = new List<Result>(tunnelPaths.Length);
 
-            var resultList = new List<Result>();
-            foreach (var filePath in filePaths)
+            foreach (var tunnelPath in tunnelPaths)
             {
                 resultList.Add(new Result
                 {
-                    Title = GetFileNameWithoutExtensions(filePath),
-                    SubTitle = filePath,
+                    Title = GetFileNameWithoutExtensions(tunnelPath),
+                    SubTitle = tunnelPath,
                     IcoPath = Image,
                     Action = e =>
                     {
                         //TODO: wireguard has to run?
-                        var command = $"wireguard.exe /installtunnelservice \"{filePath}\"";
-                        ProcessStartInfo info = new()
-                        {
-                            FileName = "cmd.exe",
-                            Verb = "runas",
-                            Arguments = $"/c {command}",
-                            UseShellExecute = true,
-                            WindowStyle = ProcessWindowStyle.Hidden
-                        };
-
-                        Process.Start(info);
-
+                        getProcess(tunnelPath, Command.install);
                         return true;
                     }
                 });
@@ -51,9 +42,16 @@ namespace Flow.Launcher.Plugin.WireGuard
         public void Init(PluginInitContext context)
         {
             Context = context;
-            if (!Directory.Exists(tunnelPaths))
+            if (!Directory.Exists(configPath))
             {
-                throw new Exception($"Plugin Wireguard: {tunnelPaths} not found.");
+                //Log.Exception($"Plugin Wireguard: {configPath} not found."); //TODO: log
+            }
+            else
+            {
+                tunnelPaths = Directory.GetFiles(configPath);
+                tunnelPaths = Array.FindAll(tunnelPaths, tunnelPath =>
+                    tunnelPath.EndsWith(".conf", StringComparison.OrdinalIgnoreCase) ||
+                    tunnelPath.EndsWith(".conf.dpapi", StringComparison.OrdinalIgnoreCase));
             }
         }
 
@@ -71,5 +69,36 @@ namespace Flow.Launcher.Plugin.WireGuard
         {
             return Path.GetFileNameWithoutExtension(filePath.EndsWith(".conf.dpapi") ? Path.GetFileNameWithoutExtension(filePath) : filePath);
         }
+
+        private void getProcess(string tunnelPath, Command commandType){
+            string command = "";
+            if (commandType == Command.install){
+                command = $"wireguard.exe /installtunnelservice \"{tunnelPath}\"";
+            } else if (commandType == Command.uninstall){
+                var tunnelName = GetFileNameWithoutExtensions(tunnelPath);
+                command = $"wireguard.exe /uninstall \"{tunnelName}\"";
+            }
+            ProcessStartInfo info = new()
+            {
+                FileName = "cmd.exe",
+                Verb = "runas",
+                Arguments = $"/c {command}",
+                UseShellExecute = true,
+                WindowStyle = ProcessWindowStyle.Hidden
+            };
+
+            try{
+                Process.Start(info);
+            } catch (Exception e) {
+                //Log.Error($"Failed to install tunnel service for {tunnelPath}");
+                //Log.Exception(e);
+            }
+        }
     }
+
+    enum Command
+{
+    install,    
+    uninstall
+}
 }
